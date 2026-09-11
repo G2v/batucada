ba04727b343ab61d950847bee3f43117d86b5c82
G2v <gdevivies@gmail.com>
2026-09-11 16:38:10 +0200

    Remove debug log for cache response
    Remove console log statement for cache and response.

--- modules/interface_instruments.js
+++ modules/interface_instruments.js
@@ -53,7 +53,6 @@ export default class InterfaceInstruments {
 		this.#instrumentsLibraryName.textContent = `${library.name} ${library.version}`;
 		const cache = await caches.open(this.#dataCache);
 		const response = await cache.match(this.#instrumentsMetadataFile);
-		console.log({cache, response })
 		this.#instrumentsRestoreButton.disabled = !response;
 	}
 
