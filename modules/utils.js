5996337e2c15530b6c721b51c29cba373401ab7e
G2v <gdevivies@gmail.com>
2026-09-14 15:59:40 +0200

    perf: CLS improvement

--- modules/utils.js
+++ modules/utils.js
@@ -7,12 +7,11 @@ export function defer(task, timeout = 3000) {
 }
 
 export async function fetchFromCache(cacheName, filename, cacheResponse = false) {
-	const url     = new URL(filename, location.href).href;
-	const network = fetch(url);
-	const cached  = await caches.open(cacheName)
-		.then(cache => cache.match(url)).catch(() => null);
-	if (cached) { network.catch(() => {}); return cached; }
-	const networkResponse = await network;
+	const url    = new URL(filename, location.href).href;
+	const cache  = await caches.open(cacheName);
+	const cached = await cache.match(url);
+	if (cached) return cached;
+	const networkResponse = await fetch(url);
 	if (!networkResponse.ok) return networkResponse;
 	const headers = new Headers(networkResponse.headers);
 	headers.delete('last-modified');
