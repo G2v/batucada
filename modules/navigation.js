7c5c464e053d8588c995c6a6954bf49fe9179de1
G2v <gdevivies@gmail.com>
2026-09-25 15:47:12 +0200

    Prevent unwanted scroll

--- modules/navigation.js
+++ modules/navigation.js
@@ -1,4 +1,4 @@
-import { defer } from './utils.js';
+import { encodeUrl, moveTrack } from './navigation_encode.js';
 import { decode, decodeAll, initialState } from './navigation_decode.js';
 
 export class Navigation {
@@ -6,8 +6,8 @@ export class Navigation {
 	#state;
 	#events;
 	#config;
-	#worker = null;
 	#searchParams;
+	#instrumentsBase;
 
 	constructor({ bus, config }) {
 		this.#bus          = bus;
@@ -15,6 +15,9 @@ export class Navigation {
 		this.#events       = config.events;
 		this.#state        = initialState(config);
 		this.#searchParams = new URLSearchParams(location.search);
+		this.#instrumentsBase = config.instrumentsLibraryReady.then(({ instruments }) =>
+			Object.fromEntries(instruments.map(({ id, strokes }) => [id, strokes.length + 1]))
+		);
 
 		history.scrollRestoration = 'manual';
 
@@ -25,52 +28,6 @@ export class Navigation {
 		this.#bus.addEventListener(this.#events.presetsPresetSelected, ({ detail }) => this.#presetSelected(detail));
 		this.#bus.addEventListener(this.#events.interfaceReset,        ({ detail }) => this.#reset());
 		this.#bus.addEventListener(this.#events.interfaceMoveTrack,    ({ detail }) => this.#moveTrack(detail));
-
-		defer(() => void this.#encoder);
-	}
-
-	get #encoder() {
-		return this.#worker ??= this.#createWorker();
-	}
-
-	async #createWorker() {
-		const worker = new Worker(new URL('./navigation_worker.js', import.meta.url), { type: 'module' });
-		worker.onmessage = (event) => this.#handleWorkerMessage(event.data);
-		worker.postMessage({ action: 'init', payload: { config: await this.#encoderConfig() } });
-		return worker;
-	}
-
-	async #encoderConfig() {
-		const { instruments } = await this.#config.instrumentsLibraryReady;
-		const instrumentsBase = Object.fromEntries(instruments.map(({ id, strokes }) => [id, strokes.length + 1]));
-
-		return Object.freeze({
-			instrumentsBase,
-			formatDigits:          this.#config.formatDigits,
-			resolution:            this.#config.resolution,
-			emptyStroke:           this.#config.emptyStroke,
-			tracksLength:          this.#config.tracksLength,
-			defaultGain:           this.#config.defaultGain,
-			defaultBars:           this.#config.defaultBars,
-			defaultBeats:          this.#config.defaultBeats,
-			defaultSteps:          this.#config.defaultSteps,
-			defaultTempo:          this.#config.defaultTempo,
-			defaultPhrase:         this.#config.defaultPhrase,
-			defaultSetValue:       this.#config.defaultSetValue,
-			defaultTitleValue:     this.#config.defaultTitleValue,
-			defaultInstrument:     this.#config.defaultInstrument,
-			defaultVolume:         this.#config.defaultVolume,
-			setSearchParam:        this.#config.setSearchParam,
-			tempoSearchParam:      this.#config.tempoSearchParam,
-			titleSearchParam:      this.#config.titleSearchParam,
-			volumeSearchParam:     this.#config.volumeSearchParam,
-			barsIndex:             this.#config.barsIndex,
-			beatsIndex:            this.#config.beatsIndex,
-			stepsIndex:            this.#config.stepsIndex,
-			phraseIndex:           this.#config.phraseIndex,
-			trackFormatSeparator:  this.#config.trackFormatSeparator,
-			trackFormatAllocation: this.#config.trackFormatAllocation,
-		});
 	}
 
 	#updateState(values) {
@@ -93,12 +50,6 @@ export class Navigation {
 		const decoder = action === 'decodeAll' ? decodeAll : decode;
 		const changes = decoder(this.#config, this.#searchParams, this.#state);
 		if (changes) this.#dispatchDecoded(changes);
-	}
-
-	#handleWorkerMessage({ action, payload }) {
-		if (action !== 'encoded') return;
-		this.#searchParams = new URLSearchParams(payload);
-		this.#navigate({ action: 'encoded', dispatch: true });
 	}
 
 	#handleNavigation(event) {
@@ -146,15 +97,15 @@ export class Navigation {
 		const { setSearchParam, titleSearchParam, defaultSetValue, defaultTitleValue } = this.#config;
 		this.#searchParams.set(setSearchParam, value || defaultSetValue);
 		this.#searchParams.set(titleSearchParam, name || defaultTitleValue);
-		this.#navigate({ action: 'decode', dispatch: true, isUnsaved: false });
+		this.#navigate({ action: 'decode', dispatch: true });
 	}
 
 	#presetsUpdated({ source, title }) {
 		if (title !== undefined) this.#encodeURL({ title });
-		if (source === 'set' || source === 'unset') this.#markEntry(source === 'unset');
+		if (source === 'set' || source === 'unset') this.#updateEntry(source === 'unset');
 	}
 
-	#markEntry(isUnsaved) {
+	#updateEntry(isUnsaved) {
 		const state = navigation.currentEntry?.getState() ?? {};
 		if (!!state.isUnsaved === isUnsaved) return;
 		navigation.updateCurrentEntry({ state: { ...state, isUnsaved } });
@@ -162,13 +113,13 @@ export class Navigation {
 
 	#encodeURL(values) {
 		this.#updateState(values);
-		this.#postMessage('encode', values);
+		this.#encode(encodeUrl, values);
 	}
 
 	#moveTrack(moved) {
 		const previousOrder = this.#state.order;
 		this.#state.order = moved.order;
-		this.#postMessage('move', { ...moved, previousOrder });
+		this.#encode(moveTrack, { ...moved, previousOrder });
 	}
 
 	#reset() {
@@ -176,7 +127,7 @@ export class Navigation {
 		for (const param of [setSearchParam, titleSearchParam, tempoSearchParam, volumeSearchParam]) {
 			this.#searchParams.delete(param);
 		}
-		this.#navigate({ action: 'reset', dispatch: false, isUnsaved: false });
+		this.#navigate({ action: 'reset', dispatch: false });
 	}
 
 	#navigate(state) {
@@ -195,15 +146,16 @@ export class Navigation {
 		navigation.navigate(nextUrl, { history: isUnsaved ? 'replace' : 'push', state });
 	}
 
-	#postMessage(action, values) {
-		queueMicrotask(() => {
-			const payload = {
-				searchParams: Object.fromEntries(this.#searchParams),
-				state: structuredClone(this.#state),
-				values,
-			};
-			this.#encoder.then(worker => worker.postMessage({ action, payload }));
+	async #encode(encoder, values) {
+		const instrumentsBase = await this.#instrumentsBase;
+		const searchParams = encoder({ ...this.#config, instrumentsBase }, {
+			searchParams: Object.fromEntries(this.#searchParams),
+			state: this.#state,
+			values,
 		});
+		if (!searchParams) return;
+		this.#searchParams = new URLSearchParams(searchParams);
+		this.#navigate({ action: 'encoded', dispatch: true });
 	}
 
 	get #url() {
@@ -211,5 +163,4 @@ export class Navigation {
 		url.search = this.#searchParams.toString();
 		return url.href;
 	}
-
 }
